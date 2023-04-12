744693ec2bfacca6f996cbe6494f71d9fc7f1693
Shreeman Arjun Sahu <shreemanarjunsahu@gmail.com>
2023-04-12 16:52:46 +0530

    🐛  pick dattime btn disabled by deault

--- lib/features/home/view/widgets/date_time_btn.dart
+++ lib/features/home/view/widgets/date_time_btn.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:velocity_x/velocity_x.dart';
 
-class PickDateTimeBtn extends StatelessWidget {
+class PickDateTimeBtn extends ConsumerWidget {
   const PickDateTimeBtn({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return ElevatedButton(
       style: ElevatedButton.styleFrom(
           elevation: 0,
           backgroundColor: Colors.black,
           minimumSize: Size.fromHeight(44)),
-      onPressed: null,
+      onPressed: () {},
       child: "Proceed to Pick Date and Time"
           .text
           .textStyle(TextStyle(
